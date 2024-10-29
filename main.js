9fc82f18fa92837dbc5569e0bdcba97fa7be4223
sngcobo023 <sngcobo023@student.wethinkcode.co.za>
2024-10-29 13:33:57 +0200

    ya

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Helpers
-
+//Testing something 
 // const lightHelper = new THREE.PointLightHelper(pointLight)
 // const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper)
